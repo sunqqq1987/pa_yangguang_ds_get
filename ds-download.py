076db250f1a683f463a27c2c2ddb880d8ef99809
sunqqq1987 <sunqqq1987@163.com>
2018-05-05 19:18:54 +0800

    修改注释

--- ds-download.py
+++ ds-download.py
@@ -1,9 +1,12 @@
 # -*- encoding:utf-8 -*-
-
+# 用法如下
 from DSAPI import DownloadStation
-
+# 输入群晖的url,用户名,密码
 ds = DownloadStation('url', 'usr', 'psw')
+# 显示下载任务列表
 ds.showTask()
+# 增加下载任务
+ds.createTask('download_url')
 ds.logout()
 
 
